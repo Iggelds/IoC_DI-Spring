f9c3396d150132092310ff492bc680c45227f19b
Aleksandr Guido <iggelds@gmail,com>
2022-11-14 21:32:33 +0100

    annotations Scope and Value

--- src/main/java/org/example/RunSpring.java
+++ src/main/java/org/example/RunSpring.java
@@ -19,8 +19,18 @@ public class RunSpring {
 //        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 //        musicPlayer.playMusic();
 
-        Computer computer = context.getBean("computer", Computer.class);
-        System.out.println(computer);
+//        Computer computer = context.getBean("computer", Computer.class);
+//        System.out.println(computer);
+
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());
+
+        ClassicalMusic classicalMusic1 = context.getBean("classicalMusic", ClassicalMusic.class);
+        ClassicalMusic classicalMusic2 = context.getBean("classicalMusic", ClassicalMusic.class);
+
+        System.out.println(classicalMusic2 == classicalMusic1);
         context.close();
     }
 }
